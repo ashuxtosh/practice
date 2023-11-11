620e3a6c2f112f7c5799fd8ed8729cacf9aad5ed
Ashutosh <ashuxtosh@gmail.com>
2023-11-11 20:58:06 +0530

    test

--- helo.py
+++ helo.py
@@ -4,3 +4,11 @@ def print():
 
 def greet():
     print("Greetings, world!")
+
+def main():
+    print()
+    print("This program prints a greeting.")
+    print()
+    print("Hello, world!")
+    print("Greetings, world!")
+    print()
